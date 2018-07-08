8139a64286a9e3df0651c27d1b20257a9191d1c4
Luan Gjokaj <luan@riangle.com>
2018-07-08 20:37:34 +0200

    Loading

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Loading.css';
 import logo from '../../assets/images/logo.svg';
+import AppearAfter from '../../Components/AppearAfter';
 
 function Loading() {
 	return (
-		<div className={styles.loading}>
-			<img src={logo} alt="Loading Logo" />
+		<div>
+			<AppearAfter delay={300} className={styles.loading}>
+				<div>
+					<img src={logo} alt="Loading Logo" />
+				</div>
+			</AppearAfter>
 		</div>
 	);
 }
