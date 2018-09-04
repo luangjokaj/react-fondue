401a0c923cd40808c38f38ddcf9f5f670e888a7b
Luan Gjokaj <luan@riangle.com>
2018-09-04 13:51:00 +0200

    Update dependencies Switch to postcss-cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
 	plugins: [
 		require('postcss-flexbugs-fixes'),
 		require('postcss-import'),
-		require('postcss-cssnext'),
+		postcssPresetEnv({
+			stage: 0,
+			features: {
+				'nesting-rules': true,
+				'color-mod-function': true,
+				'custom-media': true,
+			},
+		}),
 	],
 };
