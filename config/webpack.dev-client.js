20cb9da33daad8d346481b68a4256d1feffb5722
Luan Gjokaj <luan@riangle.com>
2019-08-02 15:29:08 +0200

    👌 IMPROVE: Remove comments

--- config/webpack.dev-client.js
+++ config/webpack.dev-client.js
@@ -70,9 +70,9 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				use: [
-					"style-loader", // creates style nodes from JS strings
-					"css-loader", // translates CSS into CommonJS
-					"sass-loader", // compiles Sass to CSS, using Node Sass by default
+					"style-loader",
+					"css-loader",
+					"sass-loader",
 				],
 			},
 			{
--- config/webpack.prod-client.js
+++ config/webpack.prod-client.js
@@ -82,9 +82,9 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				use: [
-					'style-loader', // creates style nodes from JS strings
-					'css-loader', // translates CSS into CommonJS
-					'sass-loader', // compiles Sass to CSS, using Node Sass by default
+					'style-loader',
+					'css-loader',
+					'sass-loader',
 				],
 			},
 			{
