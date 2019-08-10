222ec2b20cab230d7d1b8fe87b36dbe8dd60a624
Luan Gjokaj <luan@riangle.com>
2019-08-10 22:20:03 +0200

    👌 IMPROVE: Meta

--- src/Components/Head.tsx
+++ src/Components/Head.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-const favicon = require( '../assets/img/favicon.ico');
+const favicon = require('../assets/img/favicon.ico');
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -22,14 +22,14 @@ function Head({
 			<meta http-equiv="" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="manifest" href="/manifest.json" />
-			<link 
-				rel="apple-touch-icon" 
-				sizes="192x192" 
+			<link
+				rel="apple-touch-icon"
+				sizes="192x192"
 				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693542/cherry/reactfondue/icon192.png"
 			/>
-			<link 
-				rel="apple-touch-icon" 
-				sizes="512x512" 
+			<link
+				rel="apple-touch-icon"
+				sizes="512x512"
 				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693640/cherry/reactfondue/512.png"
 			/>
 			{description && <meta name="description" content={description} />}
@@ -37,8 +37,15 @@ function Head({
 			{description && <meta property="og:description" content={description} />}
 			{image && <meta property="og:image" content={image} />}
 			<link rel="shortcut icon" href={favicon} type="image/x-icon" />
-			<link rel="icon" sizes="192x192" href="https://i.postimg.cc/FRsbCkJ1/192.jpg" />
-			<link rel="apple-touch-icon-precomposed" href="https://i.postimg.cc/FRsbCkJ1/192.jpg" />
+			<link
+				rel="icon"
+				sizes="192x192"
+				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565467526/cherry/reactfondue/icon192.png"
+			/>
+			<link
+				rel="apple-touch-icon-precomposed"
+				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565467526/cherry/reactfondue/icon192.png"
+			/>
 			{children && children}
 			{title && <title>{title}</title>}
 		</Helmet>
