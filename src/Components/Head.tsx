8c03dfb2ce08a35ac49cc24f950551b2c9e281dd
Luan Gjokaj <luan@riangle.com>
2019-08-16 12:34:07 +0200

    👌 IMPROVE: Cleanup

--- src/Components/Head.tsx
+++ src/Components/Head.tsx
@@ -24,16 +24,26 @@ function Head({
 				name="viewport"
 				content="width=device-width, initial-scale=1"
 			/>
+			<link rel="shortcut icon" href={favicon} type="image/x-icon" />
 			<link rel="manifest" href="/manifest.json" />
+			<link
+				rel="apple-touch-icon"
+				sizes="512x512"
+				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693640/cherry/reactfondue/512.png"
+			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="192x192"
 				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693542/cherry/reactfondue/icon192.png"
 			/>
 			<link
-				rel="apple-touch-icon"
-				sizes="512x512"
-				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693640/cherry/reactfondue/512.png"
+				rel="apple-touch-icon-precomposed"
+				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png"
+			/>
+			<link
+				rel="icon"
+				sizes="192x192"
+				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png"
 			/>
 			{description && <meta name="description" content={description} />}
 			{title && <meta property="og:title" content={title} />}
@@ -41,16 +51,6 @@ function Head({
 				<meta property="og:description" content={description} />
 			)}
 			{image && <meta property="og:image" content={image} />}
-			<link rel="shortcut icon" href={favicon} type="image/x-icon" />
-			<link
-				rel="icon"
-				sizes="192x192"
-				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png"
-			/>
-			<link
-				rel="apple-touch-icon-precomposed"
-				href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png"
-			/>
 			{children && children}
 			{title && <title>{title}</title>}
 		</Helmet>
