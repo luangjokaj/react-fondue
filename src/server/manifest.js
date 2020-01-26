9b3eb9c518fed2f28dc3d9f73e9bf620ce5f53f6
Luan <luan@riangle.com>
2020-01-26 02:02:13 +0100

    🐛 FIX: Manifest

--- src/server/manifest.js
+++ src/server/manifest.js
@@ -1,26 +1,23 @@
 module.exports = {
-	name: 'ReactFondue',
-	short_name: 'ReactFondue',
-	description:
-		'ReactFondue is minimal React boilerplate with support for code splitting, hot module reload and server side rendering.',
-	icons: [
+	"name": "ReactFondue",
+	"short_name": "ReactFondue",
+	"description": "ReactFondue is minimal React boilerplate with support for code splitting, hot module reload and server side rendering.",
+	"icons": [
 		{
-			src:
-				'https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png',
-			sizes: '192x192',
-			type: 'image/png',
+			"src": "https://res.cloudinary.com/deep-impact-ag/image/upload/v1565468890/cherry/reactfondue/icon192.png",
+			"sizes": "192x192",
+			"type": "image/png",
 		},
 		{
-			src:
-				'https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693640/cherry/reactfondue/512.png',
-			sizes: '512x512',
-			type: 'image/png',
+			"src": "https://res.cloudinary.com/deep-impact-ag/image/upload/v1564693640/cherry/reactfondue/512.png",
+			"sizes": "512x512",
+			"type": "image/png",
 		},
 	],
-	background_color: '#000000',
-	theme_color: '#000000',
-	display: 'standalone',
-	orientation: 'portrait',
-	start_url: '.',
-	scope: '/',
+	"background_color": "#000000",
+	"theme_color": "#000000",
+	"display": "standalone",
+	"orientation": "portrait",
+	"start_url": ".",
+	"scope": "/",
 };
