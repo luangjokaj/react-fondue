8cc3500f9b8df97969c3438f6326c99f1fbdea73
Luan <luan@riangle.com>
2020-02-10 19:31:13 +0100

    👌 IMPROVE: Wait for dependencies to finish

--- src/server/express.js
+++ src/server/express.js
@@ -17,15 +17,14 @@ const isDev = !isProd;
 const PORT = process.env.PORT || 8080;
 let isBuilt = false;
 
-server.listen(PORT, () => {
-	isBuilt = true;
-	console.log(
-		`Server listening on \x1b[42m\x1b[1mhttp://localhost:${PORT}\x1b[0m in \x1b[41m${process.env.NODE_ENV}\x1b[0m 🌎...`,
-	);
-});
-
 const done = () => {
-	!isBuilt && console.log('Done');
+	!isBuilt &&
+		server.listen(PORT, () => {
+			isBuilt = true;
+			console.log(
+				`Server listening on \x1b[42m\x1b[1mhttp://localhost:${PORT}\x1b[0m in \x1b[41m${process.env.NODE_ENV}\x1b[0m 🌎...`,
+			);
+		});
 };
 
 if (isDev) {
